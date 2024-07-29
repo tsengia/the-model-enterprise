b5c6738ee9d2c989c90ae4bcd3f9016f54706261
Tyler Sengia <tylersengia@gmail.com>
2024-07-29 22:37:35 +0000

    Generate tfvars in tf directory

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -83,5 +83,5 @@ dbtable=$(aws cloudformation describe-stacks ${PROFILE_FLAGS} ${REGION_FLAGS} --
 echo "S3 Bucket: $bucket"
 echo "DynamoDB table: $dbtable"
 
-echo "AWS_REGION=\"${region}\"" > terraform.tfvars
-echo "STATE_S3_BUCKET_NAME=\"${bucket}\"" >> terraform.tfvars
+echo "AWS_REGION=\"${region}\"" > tf/terraform.tfvars
+echo "STATE_S3_BUCKET_NAME=\"${bucket}\"" >> tf/terraform.tfvars
