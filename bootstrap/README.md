9d8514e94c6ffc25f6eaacdf6de9f12218e29dbb
Tyler Sengia <tylersengia@gmail.com>
2024-07-29 19:46:11 +0000

    Revise bootstrap scripts to be more flexible

---
+++ bootstrap/README.md
@@ -0,0 +1,4 @@
+# Bootstrap S3 Backend
+This directory contains a `bootstrap.sh` and `bootstrap.yaml` file that are modified from this repository: [https://github.com/aws-samples/bootstrap-amazon-s3-remote-backend-for-open-tofu/tree/7c9e0b486c4a8e378443de6e4f204722cf9b70a2](https://github.com/aws-samples/bootstrap-amazon-s3-remote-backend-for-open-tofu/tree/7c9e0b486c4a8e378443de6e4f204722cf9b70a2).
+
+The source repo is MIT-0 Licensed.
---
+++ bootstrap/bootstrap.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+set -e
+stage="dev"
+region=""
+REGION_FLAGS=""
+profile=""
+PROFILE_FLAGS=""
+
+usage() {
+    echo "Usage: ${0} [--help] [--region REGION] [--stage STAGE] [--profile PROFILE]"
+    echo ""
+    echo "Where:"
+    echo "    REGION            The AWS region to create the S3 bucket and Dynamo DB tables"
+    echo "    STAGE             The deployment type. Default: ${stage}"
+    echo "    PROFILE           The AWS CLI profile to assume when calling AWS CLI v2."
+    echo ""
+    echo "This script will deploy the cloud formation template at ./bootstrap.yaml to the specified region."
+    echo "This bootstrap template has settings to create and configure an S3 Bucket, DynamoDB table, and KMS key for OpenTofu's S3 backend."
+    echo "This script requires OpenTofu to be installed and available via the PATH."
+    echo ""
+}
+
+if [[ "${1}" == "-h" ]] || [[ "${1}" == "--help" ]] || [[ "${1}" == "help" ]]; then
+    usage;
+    exit 0
+fi
+
+while [ $# -gt 0 ]; do
+    case ${1} in
+        "--profile")
+        profile=${2}
+        PROFILE_FLAGS="--profile ${profile}"
+        shift
+        shift
+        ;;
+        "--region")
+        region=${2}
+        REGION_FLAGS="--region ${region}"
+        shift
+        shift
+        ;;
+        "--stage")
+        stage=${2}
+        shift
+        shift
+        ;;
+        *) 
+        echo "ERROR: Unrecognized option ${1} !"
+        usage;
+        exit 1;
+        ;;
+    esac 
+done
+
+# Check if open-tofu (tofu) is installed
+if ! command -v tofu &> /dev/null
+then
+    echo "Error: open-tofu (tofu) is not installed. Please install it and try again."
+    exit 1
+fi
+
+# Check for valid AWS credentials
+if ! aws sts get-caller-identity &> /dev/null
+then
+    echo "Error: Invalid AWS credentials. Please configure your AWS credentials and try again."
+    exit 1
+fi
+
+if [ ! -d bootstrap ] || [ ! -f bootstrap/bootstrap.yaml ]; then
+    echo "Error: This script must be run from the top level directory of this repo!"
+    exit 1
+fi
+
+stackName="${stage}-open-tofu-remote-backend"
+
+echo "--- CloudFormation deploy ---"
+aws cloudformation deploy --template-file ./bootstrap/bootstrap.yaml --stack "$stackName" ${PROFILE_FLAGS} ${REGION_FLAGS}
+
+echo "--- Get S3 bucket & DynamoDB table name for OpenTofu backend ---"
+bucket=$(aws cloudformation describe-stacks ${PROFILE_FLAGS} ${REGION_FLAGS} --query "Stacks[?StackName=='$stackName'][].Outputs[?OutputKey=='OpenTofuBackendBucketName'].OutputValue" --output text)
+dbtable=$(aws cloudformation describe-stacks ${PROFILE_FLAGS} ${REGION_FLAGS} --query "Stacks[?StackName=='$stackName'][].Outputs[?OutputKey=='OpenTofuBackendDynamoDBName'].OutputValue" --output text)
+
+echo "S3 Bucket: $bucket"
+echo "DynamoDB table: $dbtable"
+
+echo "AWS_REGION=\"${region}\"" > terraform.tfvars
+echo "STATE_S3_BUCKET_NAME=\"${bucket}\"" >> terraform.tfvars
